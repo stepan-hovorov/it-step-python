233709c816159ebca66bbba85792c1b8d8de1f9c
stepan hovorov <stepan.hovorov@gmail.com>
2024-09-18 21:28:55 +0300

    Завдання 2: Створіть міні-гру

---
+++ lesson2.py
@@ -0,0 +1,3 @@
+name="ttttuuurrrrttttlllleeee"
+print(f"{name[1]}{name[6]}{name[10]}{name[13]}{name[-7]}{name[-1]}")
+print(f"{name[-23]}{name[6:8]}{name[-10]}{name[-5:-3]}")
---
+++ дз2.py
@@ -0,0 +1,5 @@
+from random import randint
+name=input(), input(), input(), input(), input()
+Heros=name[randint(0,len(name)-1)]
+print("Пішли на місію -",Heros)
+print(Heros)
