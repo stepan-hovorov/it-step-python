f89487fd2432b349797782c2e6cf4d5eebfc4727
stepan hovorov <stepan.hovorov@gmail.com>
2024-09-20 15:56:33 +0300

    Завдання 4.4 castle

---
+++ task4.4.py
@@ -0,0 +1,20 @@
+# castle = [
+#     1,
+#     ["c"],
+#     543,
+#     "P",
+#     ["n", ["r"]],
+#     "i",
+#     [
+#         [
+#             ["s"]
+#         ]
+#     ]
+# ]
+#
+# print(castle[3])
+# print(castle[4][1][0])
+# print(castle[5])
+# print(castle[4][0])
+# print(castle[1][0])
+# print(castle[6][0][0][0])
