aad7624fdc7dfe9f55c83b9d3565cf28581378d4
stepan hovorov <stepan.hovorov@gmail.com>
2024-09-29 09:22:29 +0300

    Завдання 4.4 castle

---
+++ дз4амвв.py
@@ -0,0 +1,9 @@
+світ = {}
+person1 = input()
+person2 = input()
+person3 = input()
+світ[person1] = input(), input(),input()
+світ[person2] = input(), input(),input()
+світ[person3] = input(), input(),input()
+print(світ.keys())
+print(світ.values())
